3dd7062a99dde598472eb77f32d14fca0aa75ef1
Pramaditya Fajri Migfar <16521406@mahasiswa.itb.ac.id>
2021-11-06 07:14:36 +0700

    final keknya

--- TUBES.py
+++ TUBES.py
@@ -1,23 +1,56 @@
+# Program Web Scraping Data Covid-19
+# Menampilkan data covid-19 di Indonesia yang telah diambil dan diolah
+# dari server API dengan menggunakan metode web scraping
+
 import requests
 import json
 
-Indonesia = "https://data.covid19.go.id/public/api/update.json"
+# KAMUS
+# Indonesia             : string
+# DKI_Jakarta           : string
+# Jawa_Barat            : string
+# Jawa_Tengah           : string
+# Jawa_Timur            : string
+# Kalimantan_Timur      : string
+# isRunning             : bool
+# inputLokasi           : int
+# inputPengguna         : int
+# indeks                : int
+# lanjut                : string
+# inputProvinsi         : int
+# namaProvinsi          : string
+# tanggalAwalKasus      : string
+# inputPenggunaProvinsi : int
 
-DKI_Jakarta = "https://data.covid19.go.id/public/api/prov_detail_DKI_JAKARTA.json"
-Jawa_Barat = "https://data.covid19.go.id/public/api/prov_detail_JAWA_BARAT.json"
-Jawa_Tengah = "https://data.covid19.go.id/public/api/prov_detail_JAWA_TENGAH.json"
-Jawa_Timur = "https://data.covid19.go.id/public/api/prov_detail_JAWA_TIMUR.json"
-Kalimatan_Timur = "https://data.covid19.go.id/public/api/prov_detail_KALIMANTAN_TIMUR.json"
-
-# Function untuk mengakses data di server API
+# Fungsi data_covid
 def data_covid(endpoint):
+    # Mengakses data di server API
+
+    # KAMUS LOKAL
+    # response  : requests.models.Response
+    # data      : dictionary
+
+    # ALGORITMA
     response = requests.get(endpoint)
     data = json.loads(response.text)
     return data
 
 # DATA INDONESIA
-# Data untuk total Indonesia
+# Fungsi TotalIndo
 def TotalIndo():
+    # Menghasilkan data total covid-19 di Indonesia
+
+    # KAMUS LOKAL
+    # update            : dictionary
+    # total, penambahan : int
+    # tanggal           : string
+    # total_positif     : int
+    # total_meninggal   : int
+    # total_sembuh      : int
+    # total_dirawat     : int
+    # output_total_indo : string
+
+    # ALGORITMA
     update = data_covid(Indonesia).get("update")
     total = update.get("total")
     penambahan = update.get("penambahan")
@@ -29,8 +62,21 @@ def TotalIndo():
     output_total_indo = f"Data Total Covid hingga tanggal {tanggal} : \nJumlah Positif : {total_positif}\nJumlah Dirawat : {total_dirawat}\nJumlah Sembuh : {total_sembuh}\nJumlah Meninggal : {total_meninggal}\n"
     return output_total_indo
 
-# Data untuk pertambahan Indonesia
+# Fungsi PertambahanHarianIndo
 def PertambahanHarianIndo():
+    # Menghasilkan data pertambahan harian covid-19 di Indonesia
+
+    # KAMUS LOKAL
+    # update                : dictionary
+    # penambahan            : int
+    # tanggal               : string
+    # penambahan_positif    : int
+    # penambahan_meninggal  : int
+    # penambahan_sembuh     : int
+    # penambahan_dirawat    : int
+    # output_harian_indo    : string
+
+    # ALGORITMA
     update = data_covid(Indonesia).get("update")
     penambahan = update.get("penambahan")
     tanggal = penambahan.get("tanggal")
@@ -41,9 +87,22 @@ def PertambahanHarianIndo():
     output_harian_indo = f"Data Penambahan Kasus Covid pada tanggal {tanggal} : \nPertambahan Kasus Positif : {penambahan_positif}\nPertambahan Kasus Dirawat : {penambahan_dirawat}\nPertambahan Kasus Sembuh : {penambahan_sembuh}\nPertambahan Kasus Meninggal : {penambahan_meninggal}\n"
     return output_harian_indo
 
-# Data untuk harian Indonesia
-
+# Fungsi PertambahanHariTertentuIndo
 def PertambahanHariTertentuIndo(indeks):
+    # Menghasilkan data pertambahan harian tertentu
+    # covid-19 di Indonesia dari indeks
+
+    # KAMUS LOKAL
+    # update                    : dictionary
+    # arrHarian                 : array of dictionary
+    # arr_positif_harian        : array of int
+    # temp                      : int
+    # arr_dirawat_harian        : array of int
+    # arr_sembuh_harian         : array of int
+    # arr_meninggal_harian      : array of int
+    # output_hari_tertentu_indo : string
+
+    # ALGORITMA
     update = data_covid(Indonesia).get("update")
     # Memasukkan data positif harian ke array data harian positif
     arrHarian = update.get("harian")
@@ -79,8 +138,21 @@ def PertambahanHariTertentuIndo(indeks):
 
 
 # DATA TOP 5 BESAR PROVINSI
-# Data untuk total tiap provinsi
+# Fungsi TotalProvinsi
 def TotalProvinsi(namaProvinsi):
+    # Menghasilkan data total covid-19 dari namaProvinsi
+
+    # KAMUS LOKAL
+    # data                  : dictionary
+    # tanggal               : string
+    # provinsi              : string
+    # total_positif         : int
+    # total_meninggal       : int
+    # total_sembuh          : int
+    # total_dirawat         : int
+    # output_total_provinsi : string
+
+    # ALGORITMA
     data = data_covid(namaProvinsi)
     tanggal = data.get("last_date")
     provinsi = data.get("provinsi")
@@ -93,6 +165,19 @@ def TotalProvinsi(namaProvinsi):
 
 # Data untuk pertambahan tiap provinsi
 def PertambahanHarianProvinsi(namaProvinsi):
+    # Menghasilkan data pertambahan harian covid-19 dari namaProvinsi
+
+    # KAMUS LOKAL
+    # data                  : dictionary
+    # tanggal               : string
+    # provinsi              : string
+    # penambahan_positif    : int
+    # penambahan_meninggal  : int
+    # penambahan_sembuh     : int
+    # penambahan_dirawat    : int
+    # output_harian_provinsi: string
+
+    # ALGORITMA
     data = data_covid(namaProvinsi)
     tanggal = data.get("last_date")
     provinsi = data.get("provinsi")
@@ -106,6 +191,20 @@ def PertambahanHarianProvinsi(namaProvinsi):
 
 # Data untuk harian tiap provinsi
 def PertambahanHariTertentuProvinsi(namaProvinsi, indeks):
+    # Menghasilkan data pertambahan harian tertentu
+    # covid-19 dari namaProvinsi dan dari indeks
+
+    # KAMUS LOKAL
+    # data                          : dictionary
+    # provinsi                      : string
+    # arrHarian                     : array of dictionary
+    # arr_positif_harian            : array of int
+    # arr_dirawat_harian            : array of int
+    # arr_sembuh_harian             : array of int
+    # arr_meninggal_harian          : array of int
+    # output_hari_tertentu_provinsi : string
+
+    # ALGORITMA
     data = data_covid(namaProvinsi)
     provinsi = data.get("provinsi")
     arrHarian = data.get("list_perkembangan")
@@ -117,10 +216,24 @@ def PertambahanHariTertentuProvinsi(namaProvinsi, indeks):
     return output_hari_tertentu_provinsi
 
 
+# ALGORITMA PROGRAM UTAMA
+
+# Deklarasi variabel link server API per lokasi
+Indonesia = "https://data.covid19.go.id/public/api/update.json"
+
+DKI_Jakarta = "https://data.covid19.go.id/public/api/prov_detail_DKI_JAKARTA.json"
+Jawa_Barat = "https://data.covid19.go.id/public/api/prov_detail_JAWA_BARAT.json"
+Jawa_Tengah = "https://data.covid19.go.id/public/api/prov_detail_JAWA_TENGAH.json"
+Jawa_Timur = "https://data.covid19.go.id/public/api/prov_detail_JAWA_TIMUR.json"
+Kalimatan_Timur = "https://data.covid19.go.id/public/api/prov_detail_KALIMANTAN_TIMUR.json"
+
 # User Interface
+# Deklarasi variabel isRunning
 isRunning = True
 
+# Membuat looping untuk mengulang pencarian data oleh pengguna
 while (isRunning):
+    # Menentukan lokasi data covid-19 yang ingin dicari
     print("=" * 50)
     print("Data apa yang mau dicari?")
     print("1. Data di Indonesia \n2. Data di provinsi besar \n3. Keluar")
@@ -130,6 +243,7 @@ while (isRunning):
     print("")
 
     if (inputLokasi == 1):
+        # Menentukan jenis data covid-19 yang ingin dicari
         print("1. Data Total Covid di Indonesia\n2. Data Covid Harian Terbaru di Indonesia\n3. Data Covid pada tanggal tertentu di Indonesia\n4. Keluar")
         print("=" * 50)
         inputPengguna = int(input("Masukkan pilihan : "))
@@ -143,17 +257,21 @@ while (isRunning):
             print(PertambahanHarianIndo())
             print("=" * 50)
         elif (inputPengguna == 3):
+            # Deklarasi variabel indeks sebagai input data covid-19 pada hari tertentu
             indeks = int(input("Data covid hari ke (02-03-2020 adalah hari ke 1) : "))
             print(PertambahanHariTertentuIndo(indeks))
             print("=" * 50)
         elif (inputPengguna == 4):
+            # Keluar dari program
             exit()
         else:
             print("Input error!!")
             continue
 
+        # Menentukan apakah pengguna ingin lanjut mencari data atau tidak
         lanjut = input("Ingin mencari data lain? (Y/N) : ")
         if (lanjut == "N" or lanjut == "n"):
+            # Keluar dari program
             exit()
         elif (lanjut == "Y" or lanjut == "y"):
             continue
@@ -163,6 +281,7 @@ while (isRunning):
 
 
     elif (inputLokasi == 2):
+        # Menentukan provinsi manakah yang ingin dicari datanya
         print("Data di provinsi manakah yang ingin dicari?")
         print("1. DKI Jakarta \n2. Jawa Barat \n3. Jawa Tengah \n4. Jawa Timur \n5. Kalimantan Timur \n6. Keluar")
         print("=" * 50)
@@ -170,6 +289,7 @@ while (isRunning):
         print("=" * 50)
         print("")
 
+        # Deklarasi variabel namaProvinsi dan tanggalAwalKasus
         if (inputProvinsi == 1):
             namaProvinsi = DKI_Jakarta
             tanggalAwalKasus = "02-03-2020"
@@ -186,11 +306,13 @@ while (isRunning):
             namaProvinsi = Kalimatan_Timur
             tanggalAwalKasus = "15-03-2020"
         elif (inputProvinsi == 6):
+            # Keluar dari program
             exit()
         else:
             print("Input error!!")
             continue
 
+        # Menentukan jenis data covid-19 yang ingin dicari
         print("1. Data Total Covid\n2. Data Covid Harian Terbaru\n3. Data Covid pada tanggal tertentu\n4. Keluar")
         print("=" * 50)
         inputPenggunaProvinsi = int(input("Masukkan pilihan : "))
@@ -204,17 +326,21 @@ while (isRunning):
             print(PertambahanHarianProvinsi(namaProvinsi))
             print("=" * 50)
         elif (inputPenggunaProvinsi == 3):
+            # Deklarasi variabel indeks sebagai input data covid-19 pada hari tertentu
             indeks = int(input(f"Data covid hari ke ({tanggalAwalKasus} adalah hari ke 1) : "))
             print(PertambahanHariTertentuProvinsi(namaProvinsi, indeks))
             print("=" * 50)
         elif (inputPenggunaProvinsi == 4):
+            # Keluar dari program
             exit()
         else:
             print("Input error!!")
             continue
 
+        # Menentukan apakah pengguna ingin lanjut mencari data atau tidak
         lanjut = input("Ingin mencari data lain? (Y/N) : ")
         if (lanjut == "N" or lanjut == "n"):
+            # Keluar dari program
             exit()
         elif (lanjut == "Y" or lanjut == "y"):
             continue
@@ -223,6 +349,7 @@ while (isRunning):
             continue
 
     elif (inputLokasi == 3):
+        # Keluar dari program
         exit()
     else:
         print("Input error!!")
